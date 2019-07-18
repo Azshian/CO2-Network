28ee7114d7c6582118c46f9963313401a8a1cc00
Azshian <52178451+Azshian@users.noreply.github.com>
2019-07-18 10:10:59 -0600

    Update and rename MeshNode.ino to MeshNodeTemp.ino

--- MeshNodeTemp.ino
+++ MeshNodeTemp.ino
@@ -14,6 +14,15 @@
 //#include <printf.h>
 
 #define TRANSMIT_PIN   2
+#define RECEIVE_PIN    3
+
+#include <OneWire.h> 
+#include <DallasTemperature.h>
+#define ONE_WIRE_BUS 4 
+OneWire oneWire(ONE_WIRE_BUS); 
+DallasTemperature sensors(&oneWire);
+
+
 
 /**** Configure the nrf24l01 CE and CS pins ****/
 RF24 radio(8, 10);
@@ -29,18 +38,19 @@ RF24Mesh mesh(radio, network);
    This will be stored in EEPROM on AVR devices, so remains persistent between further uploads, loss of power, etc.
 
  **/
-#define nodeID 2  
+#define nodeID 2
 
 
 uint32_t displayTimer = 0;
 
 struct Data {
-  unsigned long theTime;
   uint8_t id;
-  unsigned long randNumber;
+  unsigned long theTime;
+  float theValue;
 };
 
 Data data;
+
 struct payload_t {
   unsigned long ms;
   unsigned long counter;
@@ -49,7 +59,9 @@ struct payload_t {
 void setup() {
 
   Serial.begin(9600);
+  sensors.begin(); 
   pinMode(TRANSMIT_PIN, OUTPUT);
+  pinMode(RECEIVE_PIN, OUTPUT);
   //printf_begin();
   // Set the nodeID manually
   mesh.setNodeID(nodeID);
@@ -71,7 +83,10 @@ void loop() {
     displayTimer = millis();
 
     data.theTime = displayTimer;
-    data.randNumber = random(300); 
+    sensors.requestTemperatures();
+    //Serial.print(float(sensors.getTempCByIndex(0)));
+    data.theValue = float(sensors.getTempCByIndex(0));
+    //data.randNumber = random(300); 
     // Send an 'M' type message containing the current millis()
     if (!mesh.write(&data, 'M', sizeof(data))) {
 
@@ -88,24 +103,52 @@ void loop() {
         Serial.println("Send fail, Test OK");
       }
     } else {
-       digitalWrite(TRANSMIT_PIN,HIGH);
+      digitalWrite(TRANSMIT_PIN,HIGH);
       delay(50);
       digitalWrite(TRANSMIT_PIN, LOW);    
       delay(50);
       
-      Serial.print("Send OK: "); Serial.println(displayTimer);
+      Serial.print("Send OK: "); Serial.println(data.theValue);
+    
       Serial.print(data.id);
     }
   }
-
-  while (network.available()) {
+if(network.available()){
+    //digitalWrite(SIGNAL_PIN,HIGH);
+    //delay(50);
+    //digitalWrite(SIGNAL_PIN, LOW);    
+    //delay(50);
     RF24NetworkHeader header;
-    payload_t payload;
-    network.read(header, &payload, sizeof(payload));
-    Serial.print("Received packet #");
-    Serial.print(payload.counter);
-    Serial.print(" at ");
-    Serial.println(payload.ms);
+    network.peek(header);
+    switch(header.type){
+      case 'M': 
+        network.read(header,&data,sizeof(data));
+        Serial.print(header.from_node); Serial.print(" "); 
+        Serial.print(header.to_node); Serial.print(" ");
+        Serial.print(header.id); Serial.println("");
+        Serial.print(data.id); Serial.print(" "); 
+        Serial.print(data.theTime/1000); Serial.print(" ");
+        Serial.println(data.theValue); 
+        break;
+      default: network.read(header,0,0); 
+        Serial.print("default");Serial.println(header.type);
+        break;
+    }
   }
+  
+
+//  while (network.available()) {
+//    digitalWrite(RECEIVE_PIN,HIGH);
+//    delay(50);
+//    digitalWrite(RECEIVE_PIN, LOW);    
+//    delay(50);
+//    RF24NetworkHeader header;
+//    payload_t payload;
+//    network.read(header, &payload, sizeof(payload));
+//    Serial.print("Received packet #");
+//    Serial.print(payload.counter);
+//    Serial.print(" at ");
+//    Serial.println(payload.ms);
+//  }
   
 }
